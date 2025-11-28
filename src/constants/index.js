0d4cb2b33f16b7439b35807d239b1e79b003de37
MOUCTARLAWAN <lawan.mouctar@ensi.ma>
2025-11-28 15:32:59 +0100

    Implentation de la barre de tache

--- src/constants/index.js
+++ src/constants/index.js
@@ -33,4 +33,45 @@ const navIcons = [
     imag: "/icons/mode.svg",
 },
 ];
-export { navLinks, navIcons } ;
+
+const dockApps = [
+    {
+        id: "finder",
+        name: "Portfolio",
+        icon: "finder.png",
+        canOpen: true,
+    },
+    {
+        id: "safari",
+        name: "Articles",
+        icon: "safari.png",
+        canOpen: true,
+    },
+    {
+        id: "photos",
+        name: "Gallery",
+        icon: "photos.png",
+        canOpen: true,
+    },
+    {
+        id: "contact",
+        name: "Contact",
+        icon: "contact.png",
+        canOpen: true,
+    },
+    {
+        id: "terminal",
+        name: "Skills",
+        icon: "terminal.png",
+        canOpen: true,
+    },
+    {
+        id: "trash",
+        name: "Archive",
+        icon: "trash.png",
+        canOpen: true,
+    },
+];
+
+
+export { navLinks, navIcons, dockApps } ;
